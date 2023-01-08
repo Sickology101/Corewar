636df7a10c62ef8c4ce7cf0726ee64b821b5dfa2
mangheli <mangheli@student.hive.fi>
2023-01-08 14:45:01 +0200

    ?

--- srcs/asm/main.c
+++ srcs/asm/main.c
@@ -6,7 +6,16 @@
 /*   By: marius <marius@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 14:23:15 by marius            #+#    #+#             */
-/*   Updated: 2023/01/08 14:23:18 by marius           ###   ########.fr       */
+/*   Updated: 2023/01/08 14:43:50 by marius           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "assembler.h"
+
+// The assembler is meant to read through the .s file representing a champion
+// and take the code written in assembly language and turn it in bytecode
+// that the Virtual Machine can read. 
+int	main (int argc, char **argv)
+{
+	return (0);
+}
