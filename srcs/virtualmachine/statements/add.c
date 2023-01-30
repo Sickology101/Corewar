8eba663048a6240fd1d2754b12622726f51f89f0
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-01-30 10:34:47 +0200

    fixed add function registry id issue

--- srcs/virtualmachine/statements/add.c
+++ srcs/virtualmachine/statements/add.c
@@ -19,19 +19,19 @@ void	set_add(t_data *const data, t_process *carr)
 {
 	int	arg1;
 	int	arg2;
-	int	arg3;
+	int	reg_id;
 	int	rel_pos;
 
 	print_arena(data);
 	rel_pos = 1 + g_op[carr->op_id - 1].read_types;
 	arg1 = get_arg(data, carr, &rel_pos, 0);
 	arg2 = get_arg(data, carr, &rel_pos, 1);
-	arg3 = get_arg(data, carr, &rel_pos, 2);
-	if (arg1 != -1 && arg2 != -1 && arg3 != -1)
+	reg_id = data->arena[carr->cur_pos + rel_pos] - 1;
+	if (arg1 != -1 && arg2 != -1)
 	{
-		carr->reg[arg3] = carr->reg[arg1] + carr->reg[arg2];
+		carr->reg[reg_id] = carr->reg[arg1] + carr->reg[arg2];
 	}
-	set_carry(carr, arg3);
+	set_carry(carr, reg_id);
 	set_next_op(carr, (carr->cur_pos + 5) % MEM_SIZE);
 	printf("arena now [%.2x] and next [%.2x]", data->arena[carr->cur_pos], data->arena[carr->next_operation]);
 }
