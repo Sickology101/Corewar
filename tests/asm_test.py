5dd2ee9725c7f25e643769ca0f276f31dbe4f4ac
Pavel Arkharov <parkharo@c2r2p12.hive.fi>
2023-02-11 19:13:43 +0200

    Tests updateed for school imacs

--- tests/asm_test.py
+++ tests/asm_test.py
@@ -4,9 +4,10 @@ def assembly_versions(filename):
 	path_to_file = sample_files + '/' + filename
 	output_filename = filename.replace('.s', '.cor')
 	
-	os.system('./42asm/asm ' + path_to_file)
+	os.system('../asm ' + path_to_file)
 	os.system('hexdump -v ./valid_files/' + output_filename + ' >> hex1.txt')
-	
+	os.system('rm ./valid_files/' + output_filename)
+
 	os.system('../assembler ' + path_to_file)
 	os.system('hexdump -v ./valid_files/' + output_filename + ' >> hex2.txt')
 
@@ -15,13 +16,15 @@ def assembly_versions(filename):
 		os.system('rm diff' + filename)
 
 	os.system('rm ./valid_files/' + output_filename)
-	os.system('rm ./valid_files/' + output_filename)
 	os.system('rm hex1.txt')
 	os.system('rm hex2.txt')
 
-sample_files = "valid_files"
+sample_files = "./valid_files"
 asm_42_dir = "42asm"
 our_asm_dir = "ourAsm"
 
 for filename in os.listdir(sample_files):
 	assembly_versions(filename)
+
+os.system('rm hex1.txt')
+os.system('rm hex2.txt')
